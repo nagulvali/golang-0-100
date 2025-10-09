9d9e4c328de8bef37e81a9603b86f4452d98f49c
Nagulvali Shaik <nagulvali555@gmail.com>
2025-10-09 20:51:52 +0530

    go: advanced

--- 02.advanced/02.pointers/pointers.go
+++ 02.advanced/02.pointers/pointers.go
@@ -1,15 +1,46 @@
 package main
 
+import "fmt"
+
+// Notes:
+// Go follow pass by value to the functions
+// which means in function parameters a disctict value copy passed to the function as parameter
+// that is the reason below code will not update the num variable value
+
+// func changeNum(num int) {
+// 	num = 5
+// 	fmt.Println("in changeNum: ", num)
+// }
+
+// func main() {
+// 	num := 1
+
+// 	changeNum(num)
+
+// 	fmt.Println("After changeNum: ", num)
+
+// }
 
 
-func changeNum(num int) {
+func changeNum(num *int) {
+
+	*num += 5
+
+
+	fmt.Println("In changeNum: ", *num)
 	
 }
 
 
 func main() {
 
+	num := 1
 
+	fmt.Println("Initial value: ", num)
+
+	changeNum(&num)
+
+	fmt.Println("After changeNum: ", num)
 }
 
 
