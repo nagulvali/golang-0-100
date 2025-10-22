c5a1c97a801a5781b9e0eaa716a6453a9382ad65
Nagulvali Shaik <nagulvali555@gmail.com>
2025-10-22 14:48:20 +0530

    go: mailchip mvp

--- projects/mailchimp/internal/pkg/consumer/consumer.go
+++ projects/mailchimp/internal/pkg/consumer/consumer.go
@@ -1,10 +1,12 @@
 package consumer
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net/smtp"
 	"sync"
+	"text/template"
 	"time"
 
 	tp "github.com/nagulvali/mailchimp/internal/pkg/types"
@@ -21,12 +23,40 @@ func EmailWorker(id int, ch chan tp.Recipient, wg *sync.WaitGroup) {
 		smtpHost := "localhost"
 		smtpPort := "1025"
 
-		formattedMsg := fmt.Sprintf("To: %s\r\nSubject: Test Email\r\n\n%s\r\n", recipient.Email, "Just testing our email compaign.")
-		msg := []byte(formattedMsg)
+
+		// simple formatted message
+		// formattedMsg := fmt.Sprintf("To: %s\r\nSubject: Test Email\r\n\n%s\r\n", recipient.Email, "Just testing our email compaign.")
+		// msg := []byte(formattedMsg)
+
+		// using custom template
+
+		tmpl, err := template.ParseFiles("Templates/email.tmpl")
+		if err != nil {
+			log.Fatalf("Error parsing template: %v", err)
+		}
+
+		data := tp.EmailData{
+			From: 			"no-reply@example.com",
+			To:					recipient.Email,
+			Name:       recipient.Name,
+			Subject:    "Welcome to Mailchimp Go!",
+			Body:       "We're excited to have you onboard. Explore your dashboard to start sending campaigns.",
+			ButtonText: "Go to Dashboard",
+			ButtonLink: "https://mailchimp-go.example.com/dashboard",
+		}
+
+		var buf bytes.Buffer
+		if err :=  tmpl.Execute(&buf, data); err != nil {
+			log.Fatalf("Error executing template: %v", err)
+		}
+
+
+		fmt.Println(buf.String())
 		
 		fmt.Printf("Worker %d: Sending email to %s \n", id, recipient.Email)
-		err := smtp.SendMail(smtpHost + ":" + smtpPort, nil, "noreply@vali.com", []string{recipient.Email}, msg)
+		err = smtp.SendMail(smtpHost + ":" + smtpPort, nil, "noreply@vali.com", []string{recipient.Email}, buf.Bytes())
 		if err != nil {
+			// todo: handle errors in dlq to avoid blockage
 			log.Fatal(err)
 		}
 
--- projects/mailchimp/internal/pkg/types/types.go
+++ projects/mailchimp/internal/pkg/types/types.go
@@ -5,3 +5,13 @@ type Recipient struct {
 	Name string
 	Email string
 }
+
+type EmailData struct {
+	From       string
+	To         string
+	Name       string
+	Subject    string
+	Body       string
+	ButtonText string
+	ButtonLink string
+}
