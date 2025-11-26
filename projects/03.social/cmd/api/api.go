26b4af03198d52c8b70335b60340096636dce071
Nagulvali, Shaik (Corporate, IND) <shaik.nagulvali@jpmchase.com>
2025-11-26 17:04:06 +0530

    social: chi implementation

--- projects/03.social/cmd/api/api.go
+++ projects/03.social/cmd/api/api.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
@@ -14,15 +17,38 @@ type config struct {
 	addr string
 }
 
-func (app *application) mount() *http.ServeMux {
-	mux := http.NewServeMux()
+/*
+Mux is good but to group api's third party library would do better
+chi - routing library: https://go-chi.io/#/README
+*/
+// func (app *application) mount() *http.ServeMux {
+// 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /health", app.healthCheckHandler)
+// 	mux.HandleFunc("GET /health", app.healthCheckHandler)
 
-	return mux
+// 	return mux
+// }
+
+// chi implementation
+func (app *application) mount() *chi.Mux {
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	// r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("welcome"))
+	// })
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
+
+	return r
+
 }
 
-func (app *application) run(mux *http.ServeMux) error {
+func (app *application) run(mux *chi.Mux) error {
 
 	srv := &http.Server{
 		Addr:         app.config.addr,
